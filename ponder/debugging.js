54e10c199d33e3518867e3460a1cf942366fdc7e
ksg721 <gre20018@byui.edu>
2025-05-12 10:59:26 -0600

    debugging ponder

---
+++ ponder/debugging.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+    debugger;
+    const AREA = radius * radius * PI
+    return AREA
+}
+area = circleArea(3)
+console.log("Area 1:", area)
+area = circleArea(4)
+console.log("Area 2:", area)
+
+ 
