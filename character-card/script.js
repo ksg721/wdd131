6d9f7b7d4eb2f54453264889150ba964da1ea0b6
ksg721 <gre20018@byui.edu>
2025-06-14 12:57:42 -0600

    minor changes

--- character-card/script.js
+++ character-card/script.js
@@ -22,24 +22,24 @@ const character = {
 };
 
 function renderCharacter() {
-  document.querySelector(".name").textContent = character.name;
-  document.getElementById("class").textContent = character.class;
-  document.getElementById("level").textContent = character.level;
-  document.getElementById("health").textContent = character.health;
-  document.querySelector(".image").src = character.image;
-  document.querySelector(".image").alt = character.name;
+    document.querySelector(".name").textContent = character.name;
+    document.getElementById("class").textContent = character.class;
+    document.getElementById("level").textContent = character.level;
+    document.getElementById("health").textContent = character.health;
+    document.querySelector(".image").src = character.image;
+    document.querySelector(".image").alt = character.name;
 }
 
 document.getElementById("attacked").addEventListener("click", () => {
-  const log = character.attacked();
-  document.getElementById("log").textContent = log;
-  renderCharacter();
+    const log = character.attacked();
+    document.getElementById("log").textContent = log;
+    renderCharacter();
 });
 
 document.getElementById("levelup").addEventListener("click", () => {
-  const log = character.levelUp();
-  document.getElementById("log").textContent = log;
-  renderCharacter();
+    const log = character.levelUp();
+    document.getElementById("log").textContent = log;
+    renderCharacter();
 });
 
 renderCharacter();
